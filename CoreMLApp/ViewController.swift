fb31749e1d155a59a885c8159f68e5c0ec3dae61
Daniil Davidovich <dd.jopgm@gmail.com>
2024-02-27 10:44:00 +0300

    Initial Commit

---
+++ CoreMLApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreMLApp
+//
+//  Created by Daniil Davidovich on 27.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
